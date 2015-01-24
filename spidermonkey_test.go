b62c2449204e406d95fc5f4a19e8ff1aa54bd475
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-01-25 00:22:01 +0100

    make spidermonkey_test.go use stretchr/testity/suite

--- spidermonkey_test.go
+++ spidermonkey_test.go
@@ -4,65 +4,53 @@ import (
     "testing"
 
     "github.com/stretchr/testify/assert"
-    // "github.com/stretchr/testify/suite"
+    "github.com/stretchr/testify/suite"
 )
 
-func TestDefinedFunction(t *testing.T) {
-    // defer JS_ShutDown()
-
-    rt := JS_NewRuntime()
-    defer JS_DestroyRuntime(rt)
-
-    cx := JS_NewContext(rt)
-    defer JS_DestroyContext(cx)
-
-    val, err := GetFoo(cx, "gogo() + ' wrrrld'")
-
-    assert.Equal(t, "hello wrrrld", val)
-    assert.Nil(t, err)
+type GetFooTestSuite struct {
+    suite.Suite
+    Context JSContext
+    Runtime JSRuntime
 }
 
-func TestException(t *testing.T) {
-    // defer JS_ShutDown()
-
-    rt := JS_NewRuntime()
-    defer JS_DestroyRuntime(rt)
-
-    cx := JS_NewContext(rt)
-    defer JS_DestroyContext(cx)
-
-    val, err := GetFoo(cx, "throw new Error('test')")
-
-    assert.Equal(t, "", val)
-    assert.Nil(t, err)
+func (suite *GetFooTestSuite) SetupTest() {
+    suite.Runtime = JS_NewRuntime()
+    suite.Context = JS_NewContext(suite.Runtime)
 }
 
-func TestSyntaxError(t *testing.T) {
-    // defer JS_ShutDown()
-
-    rt := JS_NewRuntime()
-    defer JS_DestroyRuntime(rt)
-
-    cx := JS_NewContext(rt)
-    defer JS_DestroyContext(cx)
-
-    val, err := GetFoo(cx, "8==;")
-
-    assert.Equal(t, "", val)
-    assert.Nil(t, err)
+func (suite *GetFooTestSuite) TeardownTest() {
+    JS_DestroyContext(suite.Context)
+    JS_DestroyRuntime(suite.Runtime)
 }
 
-func TestValidScript(t *testing.T) {
-    // defer JS_ShutDown()
+func (suite *GetFooTestSuite) TestDefinedFunction() {
+    val, err := GetFoo(suite.Context, "gogo() + ' wrrrld'")
 
-    rt := JS_NewRuntime()
-    defer JS_DestroyRuntime(rt)
+    assert.Equal(suite.T(), "hello wrrrld", val)
+    assert.Nil(suite.T(), err)
+}
 
-    cx := JS_NewContext(rt)
-    defer JS_DestroyContext(cx)
+func (suite *GetFooTestSuite) TestException() {
+    val, err := GetFoo(suite.Context, "throw new Error('test')")
 
-    val, err := GetFoo(cx, "['abc', 'def'].join('-')")
+    assert.Equal(suite.T(), "", val)
+    assert.Nil(suite.T(), err)
+}
 
-    assert.Equal(t, "abc-def", val)
-    assert.Nil(t, err)
+func (suite *GetFooTestSuite) TestSyntaxError() {
+    val, err := GetFoo(suite.Context, "8==;")
+
+    assert.Equal(suite.T(), "", val)
+    assert.Nil(suite.T(), err)
+}
+
+func (suite *GetFooTestSuite) TestValidScript() {
+    val, err := GetFoo(suite.Context, "['abc', 'def'].join('-')")
+
+    assert.Equal(suite.T(), "abc-def", val)
+    assert.Nil(suite.T(), err)
+}
+
+func TestExampleTestSuite(t *testing.T) {
+    suite.Run(t, new(GetFooTestSuite))
 }
